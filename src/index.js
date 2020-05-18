1677b6b8e7338536b12ab603e3ae4ba4e7932748
Sunghyun Lee <98dltjdgus@naver.com>
2020-05-19 03:25:37 +0900

    create first react component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Hello greeting={'Hello' + 'Reafct Nunja'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
