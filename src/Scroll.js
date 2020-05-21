e0ef2b94c91c3ce34b63a1c197f6a010f7013151
Sunghyun Lee <98dltjdgus@naver.com>
2020-05-21 21:53:44 +0900

    Add scrollable CardList feature
    카드리스트만 별도로 스크롤 가능하도록 기능을 제공하는 Scroll.js 추가.

---
+++ src/Scroll.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// childeren을 활용하여
+const Scroll = (props) => {
+    // console.log(props);
+    // 자식 노드를 가지는 경우, props는 children의 요소들을 포함하게 된다.
+    
+    return (
+        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '750px' }}>
+            { props.children }
+        </div>
+    )
+}
+
+export default Scroll;
