edca97d5deab23ec86ced3b9b0069cc40c3e9ee1
Sunghyun Lee <98dltjdgus@naver.com>
2020-05-21 21:16:15 +0900

    Add features that use robots json file on the server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { Fragment, Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
+        // App.js는 App의 상태를 설명해주는 state를 가지고 있음.
         this.state = {
-            robots: robots,
+            robots: [], // 빈 배열을 가지는 rotots
             searchfield: '',
         }
+    }
+
+    componentDidMount() {
+        // 컴포넌트가 마운트되면, robots 배열을 robots.js로 채움.
+        // Updating 발생으로 render()를 다시 실행함.
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
     }
 
     // SearchBox의 this가 아닌 App의 this를 사용하기 위해 화살표 함수를 사용.
@@ -23,15 +31,21 @@ class App extends Component {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
 
-        return (
-            <Fragment>
-                <div className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots={filteredRobots}/>
-                </div>
-            </Fragment>
-        );
+        if(this.state.robots.length === 0) {
+            // 완전히 json이 로딩되지 않아 this.state.robots의 길이가 0인 경우.
+            return <h1>Loading</h1>
+        } else {
+            // 출력할 this.state.robots가 있는 경우.
+            return (
+                <Fragment>
+                    <div className='tc'>
+                        <h1 className='f1'>RoboFriends</h1>
+                        <SearchBox searchChange={this.onSearchChange}/>
+                        <CardList robots={filteredRobots}/>
+                    </div>
+                </Fragment>
+            );
+        }
     }
 }
 
